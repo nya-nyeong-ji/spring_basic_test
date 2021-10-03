fc8c49a34a5c8efbcfc1ccfe74cf6dbc3922c958
nya-nyeon-gi <jjj7061@naver.com>
2021-10-03 12:01:04 +0900

    step-07 Embedded Tip

--- src/main/java/com/example/spring_basic_test/exception/MemberNotFoundException.java
+++ src/main/java/com/example/spring_basic_test/exception/MemberNotFoundException.java
@@ -1,11 +1,17 @@
 package com.example.spring_basic_test.exception;
 
+import com.example.spring_basic_test.domain.model.Email;
 import lombok.Getter;
 
 @Getter
 public class MemberNotFoundException extends RuntimeException{
+    private String id;
+    private Email email;
 
     public MemberNotFoundException(String id) {
-        super(id + "is not existed!");
+        this.id = id;
+    }
+    public MemberNotFoundException(Email email) {
+        this.email = email;
     }
 }
